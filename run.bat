242ccfbfc0ec820b5a42116afe655309aabe1599
Piyanan.K <duck_comen@hotmail.com>
2013-10-31 18:49:30 +0700

    fix write script choice append

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-echo off
+echo on
 rem ------------delete old result
 del *.bin
 cd StreamResult
@@ -16,10 +16,12 @@ set streamname=%cd%\getResult_Stream.str
 
 rem %date:~7,2%%date:~4,2%%date:~10,4%_%time:~0,2%%time:~3,2%%time:~6,2%.log
 set date=%date%
-set time=%time%
-set logname= %cd%\log\%date:~7,2%%date:~4,2%%date:~10,4%.%time:~0,2%%time:~3,2%%time:~6,2%.log
+set time1=%time%
+set hour=%time1:~0,2%
+if "%hour:~0,1%" == " " set hour=0%hour:~1,1%
+set logname= %cd%\log\%date:~7,2%%date:~4,2%%date:~10,4%.%hour%%time1:~3,2%%time1:~6,2%.log
 
-clemb -server -hostname %hostname% -port %port%  -username %username% -password %password%  -script %cd%\script.txt -execute %streamname% -log %logname%  
+clemb -server -hostname %hostname% -port %port%  -username %username% -password %password%  -script %cd%\script.txt -execute -log %logname%  
 @echo on
 echo Piyanan.K > finish.bin
 
