c618ef2ff2162f10e028cd27d59f79d42ca1ff21
Piyanan.K <duck_comen@hotmail.com>
2013-11-08 21:58:48 +0700

    add userform, modify log msg., set format before save

--- emailsender/EmailSender.bat
+++ emailsender/EmailSender.bat
@@ -10,11 +10,11 @@ GOTO G_EXCEPTION
 :P_EXCEPTION
 REM exception happened, notify someone
 blat %problem% -f %SENDFROM% -to %SENDTO% -server %MAILSERVERNAME% -subject "Trial Tracker batch has unexpected error." 
-blat %content% -attach %ATTACH_RESULT% -attach %ATTACH_DB_SCRIPT_LOG% -f %SENDFROM% -to %SENDTOAdmin% -server %MAILSERVERNAME% -subject "For Admin :: Trial Tracker batch has unexpected error ( %2 )."
+blat %content% -attach %ATTACH_RESULT% -attach %ATTACH_DB_SCRIPT_LOG% -f %SENDFROM% -to %SENDTOAdmin% -server %MAILSERVERNAME% -subject "For Admin :: Trial Tracker status : %subject% has unexpected error ( %2 )."
 GOTO E_CLOSE_S
 :G_EXCEPTION
 REM Batch Job complete
-blat %content% -attach %ATTACH_RESULT% -attach %ATTACH_DB_SCRIPT_LOG% -f %SENDFROM% -to %SENDTO% -cc %CCMail% -server %MAILSERVERNAME% -subject "Trial Tracker Batch completed successfully ( %2 )."  -log %logname%
+blat %content% -attach %ATTACH_RESULT% -attach %ATTACH_DB_SCRIPT_LOG% -f %SENDFROM% -to %SENDTO% -cc %CCMail% -server %MAILSERVERNAME% -subject "Trial Tracker status : %subject% ( %2 )."  -log %logname%
 
 GOTO E_CLOSE_S
 
