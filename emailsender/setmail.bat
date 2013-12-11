709431e0ec784491ddc0eaf8d4c7a127079f30c7
Piyanan.K <duck_comen@hotmail.com>
2013-12-11 10:29:16 +0700

    update log and config

--- emailsender/setmail.bat
+++ emailsender/setmail.bat
@@ -2,14 +2,14 @@ echo off
 rem : set information
 SET MAILSERVERNAME=snjhub01.sanjose.hgst.com
 SET SENDFROM=Piyanan.kamlangmark@hgst.com
-SET CCMail=Piyanan.Kamlangmark@hgst.com,Phanuwat.Jak@hitachigst.com
+SET CCMail=Piyanan.Kamlangmark@hgst.com,Phanuwat.Jak@hitachigst.com,anupong.cpe15@gmail.com
 SET SENDTOAdmin=Piyanan.Kamlangmark@hgst.com,Phanuwat.Jak@hitachigst.com,anupong.cpe15@gmail.com
-SET SENDTO=piyanan.kamlangmark@hgst.com,Ekkarach@hitachigst.com,Teppayut.Laokong@hgst.com
-SET ATTACH_RESULT=C:\Tomcat_5.5\webapps\te5\SummaryResult\J1B_6110_deta_Temp_change_to_18_dreeC\Trialtracker.xls
-SET ATTACH_DB_SCRIPT_LOG=C:\Tomcat_5.5\webapps\TrialTrackerStatus\log\20131129.222228.log
-SET content= C:\Tomcat_5.5\webapps\te5\SummaryResult\J1B_6110_deta_Temp_change_to_18_dreeC\Trialtracker_web.mht
+SET SENDTO=jare.sophanpanitchakul@hgst.com
+rem SET ATTACH_RESULT=C:\Tomcat_5.5\webapps\te5\SummaryResult\CBE0.74_Test_Time_Monitoring\Trialtracker.xls
+SET ATTACH_DB_SCRIPT_LOG=C:\Tomcat_5.5\webapps\TrialTrackerStatus\log\20131211.060200.log
+SET content= C:\Tomcat_5.5\webapps\te5\SummaryResult\CBE0.74_Test_Time_Monitoring\Trialtracker_web.mht
 SET vblog =C:\Tomcat_5.5\webapps\TrialTrackerStatus\VbaErrorLog.txt
-SET subject=J1B_6110_deta_Temp_change_to_18_dreeC
+SET subject=CBE0.74_Test_Time_Monitoring
 echo on
 
 
